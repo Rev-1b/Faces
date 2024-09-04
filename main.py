9eaddc5b4fb1dba7575e12959d785ce638ffcb10
Alex Luenko <new.luenkoaleksei@gmail.com>
2024-09-04 13:58:39 +0400

    Added links-file support

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import click
 from image_parsers import FaceRecognitionExtractor, HaarcascadesExtractor, DeepFaceExtractor
 from image_savers import FaceCleanup
 from utils import choose_video_downloader, open_folder, choose_folder
+from video_loaders import YouTubeVideoDownloader
 
 
 @click.command()
@@ -29,10 +30,17 @@ def main(normal_video_dir, deepfake_video_dir, raw_photos_dir, photos_dir, perma
 
             video_dir = deepfake_video_dir if is_deepfake else normal_video_dir
             video_downloader = choose_video_downloader(video_dir)
+
+            frame_skip = click.prompt(
+                'Выберите, сколько кадров пропускать',
+                type=int,
+                default=10,
+                show_default=True
+            )
+
             video_path, video_name = video_downloader.download()
 
-            # В этом месте выбираем, каким обработчиком изображения пользоваться
-            face_extractor = HaarcascadesExtractor(video_path, video_name, raw_photos_dir, is_deepfake)
+            face_extractor = HaarcascadesExtractor(video_path, video_name, raw_photos_dir, is_deepfake, frame_skip)
             face_extractor.process_video()
             face_extractor.save_face_data(temp_csv_file)
 
@@ -51,8 +59,57 @@ def main(normal_video_dir, deepfake_video_dir, raw_photos_dir, photos_dir, perma
             print('Начинаем сначала')
 
 
+@click.command()
+@click.option('--normal-video-dir', default=os.path.join('videos', 'normal'), help='Папка с нормальными видео.')
+@click.option('--raw_photos-dir', default='raw_photos', help='Папка для сохранения сырых изображений лиц.')
+@click.option('--photos-dir', default='photos', help='Папка для сохранения итоговых изображений лиц.')
+@click.option('--permanent-csv-file', default='meta.csv', help='CSV файл для хранения данных о лицах.')
+@click.option('--links-file', default='links.txt', help='Файл со ссылками на видео.')
+def main2(normal_video_dir, raw_photos_dir, photos_dir, permanent_csv_file, links_file):
+    with open(links_file, 'r') as file:
+        links = file.readlines()
+
+    for link in links:
+        try:
+            print(link)
+            # Разбиваем строку на ссылку и значение frame_skip
+            if not link:
+                break
+            parts = link.strip().split()
+            video_url = parts[0]
+            frame_skip = int(parts[1])
+
+            temp_csv_file = os.path.join(tempfile.gettempdir(), f"{uuid.uuid4()}.csv")
+
+            # Используем нормальную папку для всех видео
+            video_downloader = YouTubeVideoDownloader(normal_video_dir, video_url)
+
+            # Загружаем видео с использованием ссылки
+            video_path, video_name = video_downloader.download()
+
+            # Инициализируем и запускаем извлечение лиц
+            face_extractor = HaarcascadesExtractor(video_path, video_name, raw_photos_dir, False, frame_skip)
+            face_extractor.process_video()
+            face_extractor.save_face_data(temp_csv_file)
+
+            # Открываем папку для редактирования изображений
+            open_folder(raw_photos_dir)
+            click.prompt("Удалите ненужные изображения из папки raw_faces и нажмите Enter для продолжения", type=str,
+                         default="")
+
+            folder = choose_folder()
+            full_output_dir = os.path.join(photos_dir, folder)
+
+            cleanup_manager = FaceCleanup(temp_csv_file, raw_photos_dir, permanent_csv_file, full_output_dir)
+            cleanup_manager.cleanup_faces()
+        except Exception as err:
+            print(f"Ошибка при обработке видео {video_url}: {err}")
+        finally:
+            print('Обработка следующего видео')
+
+
 if __name__ == "__main__":
-    main()
+    main2()
 
 
 login = 'wolftau'
